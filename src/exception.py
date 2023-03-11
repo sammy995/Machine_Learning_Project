53d218a6df38ec92c6c50cd4d9049f0a85cb63f1
Shubham Lagad <shubhamlagad@gmail.com>
2023-03-11 11:15:07 -0600

    Data transformation done

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,7 @@
 # For exception handling
-
 import sys
 import logging
-import logger
+from src.logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
