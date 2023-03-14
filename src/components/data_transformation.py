722f2fb5a286ace548bf29b11e50138ed975cc3c
Shubham Lagad <shubhamlagad@gmail.com>
2023-03-13 21:20:58 -0500

    Model Trainer done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,15 +1,17 @@
 import sys
-import os
 from dataclasses import dataclass
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+import os
+
 from src.utils import save_object
 
 @dataclass
@@ -101,7 +103,7 @@ class DataTransformation:
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path,
+                self.data_transformation_config.preprocessor_obj_file_path
             )
         except Exception as e:
             raise CustomException(e,sys)
